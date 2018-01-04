7708be5f5d014eddeffc0e0c07688aa4aef3c0c0
Zach Orlovsky <sadorlovsky@gmail.com>
2018-01-04 05:44:05 +0300

    show movie posters while initial loading

--- app/renderer/components/Spinner.js
+++ app/renderer/components/Spinner.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { ipcRenderer } from 'electron'
 import { PulseLoader } from 'halogenium'
+import { compose, withState, lifecycle } from 'recompose'
 
-const Spinner = () => (
+const Spinner = ({ poster }) => (
   <div className='container'>
+    <div className='overlay' />
     <PulseLoader color='#F553BF' size='16px' margin='4px' />
 
     <style jsx global>{`
       body {
-        background: #282629;
         margin: 0;
         padding: 0;
       }
@@ -19,9 +21,36 @@ const Spinner = () => (
         display: flex;
         justify-content: center;
         align-items: center;
+        background-color: #282629;
+        background-image: url(https://image.tmdb.org/t/p/w500/${poster});
+        background-size: cover;
+        background-position: center;
+      }
+
+      .overlay {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 3;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
       }
     `}</style>
   </div>
 )
 
-export default Spinner
+const enhanced = compose(
+  withState('poster', 'setPoster', null),
+  lifecycle({
+    componentDidMount () {
+      ipcRenderer.on('poster', (e, { poster }) => {
+        this.props.setPoster(poster)
+      })
+    }
+  })
+)
+
+export default enhanced(Spinner)
